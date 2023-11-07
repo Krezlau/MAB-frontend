3bb9ebc2230244945bb5602ba05c633d25dbbb74
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2023-11-07 18:29:20 +0100

    search query params

--- src/app/components/pages/search-page/search-page.component.ts
+++ src/app/components/pages/search-page/search-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import ISearchableEntity from 'src/app/models/ISearchableEntity';
 import { SearchService } from 'src/app/services/search.service';
@@ -7,14 +8,34 @@ import { SearchService } from 'src/app/services/search.service';
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
 })
-export class SearchPageComponent {
+export class SearchPageComponent implements OnDestroy {
+  constructor(
+    private searchService: SearchService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.sub = this.route.queryParams.subscribe((params) => {
+      if (params['q']) {
+        this.search(params['q']);
+      }
+    });
+  }
 
-  constructor(private searchService: SearchService) {}
-
-  entities$: Observable<ISearchableEntity[]> = new Observable<ISearchableEntity[]>();
+  sub: Subscription = new Subscription();
+  entities$: Observable<ISearchableEntity[]> = new Observable<
+    ISearchableEntity[]
+  >();
   isLoading = this.searchService.isLoading;
 
-  onSearch(searchTerm: string) {
+  search(searchTerm: string) {
     this.entities$ = this.searchService.search(searchTerm);
+  }
+
+  onSearch(searchTerm: string) {
+    this.router.navigate(['search'], { queryParams: { q: searchTerm } });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
