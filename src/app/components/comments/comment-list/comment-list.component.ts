0f142b3495739c997712fcdaaaf5eafb2d83dd34
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2023-11-25 13:44:59 +0100

    comments styling and layout

--- src/app/components/comments/comment-list/comment-list.component.ts
+++ src/app/components/comments/comment-list/comment-list.component.ts
@@ -1,11 +1,21 @@
 import { Component, Input, OnInit } from '@angular/core';
 import IComment from 'src/app/models/IComment';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-comment-list',
-  templateUrl: './comment-list.component.html'
+  templateUrl: './comment-list.component.html',
 })
 export class CommentListComponent {
   @Input() comments: IComment[] | null = [];
   @Input() isLoading: boolean = false;
+
+  username: string = '';
+
+  constructor(authService: AuthService) {
+    this.username = authService.getAuthState()().username;
+  }
+
+  deleteComment(id: string) {
+  }
 }
